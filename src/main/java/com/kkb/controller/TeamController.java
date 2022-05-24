abcaadcd62b0b970627cd8141bd011a72b8443a9
xqz <1412025690@qq.com>
2022-05-24 13:04:53 +0800

    SpringMVC入门程序04 2022-05-24

--- src/main/java/com/kkb/controller/TeamController.java
+++ src/main/java/com/kkb/controller/TeamController.java
@@ -6,19 +6,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
+/**
+ * 告诉springmvc核心处理器,我是处理请求的
+ */
 @Controller
 public class TeamController {
-
     @Autowired
     private TeamService teamService;
 
-    @RequestMapping("/team")
+    @RequestMapping("hello.do")
     public ModelAndView add() {
         System.out.println("TeamController----add----");
         teamService.add();
         ModelAndView mv = new ModelAndView();
-        mv.addObject("teamName","湖人");  // 相当于request.setAttribute("teamName","湖人");
+        mv.addObject("teamName", "湖人");  // 相当于request.setAttribute("teamName","湖人");
         mv.setViewName("index");    //未来经过SpringMvc视图解析器处理,转换成 物理路径 : index.jsp
+        // 经过 internalResourceViewResolver 对象处理之后前后缀就变为   index.jsp
         return mv;
     }
 }
